c00811ed0465c9ab97598f48897acabf3607d6fe
Sifa19 <naikprachita123@gmail.com>
2025-05-31 00:05:35 +0530

    Parcel

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
-  { class: "heading", style: { color: "white", background: "grey" } },
+  { className: "heading", style: { color: "white", background: "grey" } },
   "Hello World!"
 );
 
-const child = React.createElement("div", { class: "child" }, heading);
-const sibling = React.createElement("div", { class: "sibling" });
-const twin1 = React.createElement("div", { class: "twin1" }, "twin1");
-const twin2 = React.createElement("div", { class: "twin2" }, "twin2");
-const twins = React.createElement("div", { class: "twins" }, [twin1, twin2]);
-const parent = React.createElement("div", { class: "parent" }, [
+const child = React.createElement("div", { className: "child" }, heading);
+const sibling = React.createElement("div", { className: "sibling" });
+const twin1 = React.createElement("div", { className: "twin1" }, "twin1");
+const twin2 = React.createElement("div", { className: "twin2" }, "twin2");
+const twins = React.createElement("div", { className: "twins" }, [
+  twin1,
+  twin2,
+]);
+const parent = React.createElement("div", { className: "parent" }, [
   child,
   sibling,
   twins,
