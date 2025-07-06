4ba7251e7d73afae3e7f9838e9e31c96d33adc3e
Sifa19 <naikprachita123@gmail.com>
2025-07-06 17:47:04 +0530

    Login Button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import { useState } from "../../node_modules/react";
 import { CDN_LOGO_URL } from "../utils/constants";
 
 function Header() {
+  const [login, setLogin] = useState(true);
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,7 +13,10 @@ function Header() {
           <li>Home</li>
           <li>About</li>
           <li>Contact</li>
-          <li>🛒</li>
+          <li>Cart</li>
+          <li onClick={() => setLogin((login) => !login)}>
+            {!login ? `Login` : "Logout"}
+          </li>
         </ul>
       </div>
     </div>
